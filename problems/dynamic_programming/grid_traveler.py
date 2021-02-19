6ef2c22451ebb0c3064931118f3e076d0f9a17be
Colin Valentini <valentini.colin@gmail.com>
2021-02-18 22:31:40 -0500

    Improve grid_traveler solution by using symmetry of arguments

--- problems/dynamic_programming/grid_traveler.py
+++ problems/dynamic_programming/grid_traveler.py
@@ -18,6 +18,10 @@ def get_number_of_ways(n, m, memo):
   # If we have a value already computed in the cache, return it
   if (n,m) in memo:
     return memo[(n,m)]
+  # The problem is symmetric about its arguments, so we can return
+  # return the reversed args solution if we have it
+  elif (m,n) in memo:
+    return memo[(m,n)]
   
   # Reduce the problem into two subproblems:
   # (1) The number of solutions after moving right one cell
@@ -25,4 +29,4 @@ def get_number_of_ways(n, m, memo):
   memo[(n,m)] = get_number_of_ways(n, m-1, memo) + get_number_of_ways(n-1, m, memo)
   return memo[(n,m)]
 
-print(f'number_of_ways_to_bottom_right(n=4, m=4): {number_of_ways_to_bottom_right(4, 4)}')
+print(f'number_of_ways_to_bottom_right(n=18, m=18): {number_of_ways_to_bottom_right(18, 18)}')
