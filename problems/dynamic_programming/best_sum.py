960ecf9c1f503bd5b1d2f54b049f417764812699
Colin Valentini <valentini.colin@gmail.com>
2021-02-27 18:26:46 -0500

    Add tabulation solution to best_sum problem

--- problems/dynamic_programming/best_sum.py
+++ problems/dynamic_programming/best_sum.py
@@ -1,21 +1,15 @@
-def best_sum(target, numbers):
+from enum import Enum, auto
+
+class Strategy(Enum):
+  Recursion = auto()
+  Tabulation = auto()
+
+def best_sum(target, numbers, strategy=Strategy.Tabulation):
   '''
   Returns an array A of integers (each of which is an element of `numbers`), such that
   `sum(A) == target`, and `len(A)` is minimal across all possibe solutions `A`
 
   If no solution exists, returns None
-
-  Time complexity:
-    - O(n*m^2) since:
-      * We will have at worst `m` (m=target) recursion levels deep => O(m)
-      * At each recursive level could at most branch out to `n` additional nodes => O(m * n)
-        (we don't have to fully drill into every sub-branch completely due to our memo)
-      * For each function call we have to update our memo with an array at each => O(m * n * m)
-  
-  Space complexity:
-    - O(m^2) since:
-      * We are maintaing a memo cache with at most `m` (m=target) number of keys, and
-      * Each key in the memo cache contains an array of at most `m` length
 
   >>> best_sum(300, [1, 10, 30, 50])
   [100, 100, 100]
@@ -23,9 +17,25 @@ def best_sum(target, numbers):
   >>> best_sum(300, [1, 25, 50, 150])
   [150, 150]
   '''
-  return _best_sum_recursive(target, numbers, {})
+  if strategy == Strategy.Tabulation:
+    return _best_sum_tabulative(target, numbers)
+  elif strategy == Strategy.Recursion:
+    return _best_sum_recursive(target, numbers, {})
+  
+  raise KeyError(f'Invalid strategy: {strategy}')
 
 def _best_sum_recursive(target, numbers, memo):
+  '''
+  Time complexity: O(n^2 * m) for n = target, m = len(numbers)
+    * Worst case recursion causes n levels deep (1, ..., 1 n times) => O(n)
+    * Each recursive level could branch out to at most m additional nodes => O(n * m)
+    * Each function call requires copying an array into our memo of size at most n => O(n * m * n)
+  
+  Space complexity:
+    - O(n^2) since:
+      * We are maintaing a memo cache with at most n number of keys, and
+      * Each key in the memo cache contains an array of at most n length
+  '''
   if target == 0:
     return []
   
@@ -49,7 +59,46 @@ def _best_sum_recursive(target, numbers, memo):
   memo[target] = solution
   return memo[target]
 
+def _best_sum_tabulative(target, numbers):
+  '''
+  Time complexity: O(n^2 * m) for n = target, m = len(numbers)
+    * 
+
+  Space complexity: O(n^2)
+  '''
+  table = [None] * (target + 1)
+
+  # Base case: The best way to construct zero is with no elements from numbers
+  table[0] = []
+
+  # Induction case: If we can construct a subTarget <= target amount, then
+  # we an also construct a solution for subTarget + num for each num in numbers
+  # (maintain only the minimum length solutions as we go)
+  for subTarget in range(target+1):
+
+    # If the subproblem has a solution, then we can continue
+    if table[subTarget] is not None:
+      for num in numbers:
+        superTarget = subTarget + num
+        if superTarget <= target:
+
+          # If there's already a supertarget solution, we compare
+          # the current solution to the alternative of using the 
+          # subtarget's solution + [num], choosing the shorter of the two
+          candidate = table[subTarget] + [num]
+          if table[superTarget] is not None:
+            if len(candidate) < len(table[superTarget]):
+              table[superTarget] = candidate
+
+          # Otherwise set the supertarget's solution array to the just
+          # found solution (candidate)
+          else:
+            table[superTarget] = candidate
+    
+  return table[target]
+
 # Some test examples
-print(f'best_sum(8, [2, 3, 5]): {best_sum(8, [2, 3, 5])}') # [3, 5]
-print(f'best_sum(7, [1, 2, 3]): {best_sum(7, [1, 2, 3])}') # [1, 3, 3]
-print(f'best_sum(300, [1, 10, 30, 50, 100]): {best_sum(300, [1, 10, 30, 50, 100])}') # [100, 100, 100]
+assert best_sum(8, [2, 3, 5]) == [3, 5]
+assert best_sum(7, [1, 2, 3]) == [1, 3, 3]
+assert best_sum(100, [1, 2, 5, 25, 8]) == [25, 25, 25, 25]
+assert best_sum(300, [1, 10, 30, 50, 100]) == [100, 100, 100]
