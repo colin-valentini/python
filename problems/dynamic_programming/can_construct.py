2f1dbbe6e0f482d73f5a85f276c12a4dde9cd3fe
Colin Valentini <valentini.colin@gmail.com>
2021-02-28 17:27:09 -0500

    Add initial tabulative solution for can_construct

--- problems/dynamic_programming/can_construct.py
+++ problems/dynamic_programming/can_construct.py
@@ -16,7 +16,7 @@ def can_construct(target, words):
     - O(n^2) since:
       * 
   '''
-  return _can_construct_recursive(target, words, {})
+  return _can_construct_tabulative(target, words)
 
 def _can_construct_recursive(target, words, memo):
 
@@ -34,7 +34,42 @@ def _can_construct_recursive(target, words, memo):
   memo[target] = False
   return memo[target]
 
+def _can_construct_tabulative(target, words):
+  '''
+  Time complexity: O() for n = len(target), m = len(words)
+    * 
+
+  Space complexity: O()
+  '''
+  # Use a table array to maintain solutions to subproblems, in this setup
+  # each value 'i' in the table corresponds to whether target[len(target)-i:] 
+  # can be constructed using just string from "words"
+  #
+  # Ex: target='hello', table[4] == True => 'ello' (target[5-4:]) can be 
+  # constructed from elements of "words"
+  table = [False] * (len(target) + 1)
+  
+  # Base case: Empty string
+  table[0] = True
+
+  # Induction case: If we can construct a subTarget (subTarget is a substring
+  # of target), then we can construct word + subTarget
+  for i in range(len(target)+1):
+    subTarget = target[len(target)-i:]
+    if table[i]:
+      for word in words:
+
+        # Check if adding this word to the substring creates a match with
+        # the target at the same position, if so, udpate the table array
+        superTarget = word + subTarget
+        whatSuperTargetShouldBe = target[len(target)-len(superTarget):]
+        if superTarget == whatSuperTargetShouldBe:
+          table[len(superTarget)] = True
+  
+  return table[len(target)]
+
 # Some test examples
-print(f'can_construct("abcdef", ["ab", "abc", "cd", "def", "abcd"]): {can_construct("abcdef", ["ab", "abc", "cd", "def", "abcd"])}') # True
-print(f'can_construct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]): {can_construct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])}') # False
-print(f'can_construct("e" * 35 + "f", ["e" * i for i in range(1,8)]): {can_construct("e" * 35 + "f", ["e" * i for i in range(1,8)])}') # False
+assert can_construct("abcdef", ["ab", "abc", "cd", "def", "abcd"]) == True
+assert can_construct("abcdef", ["ab", "abc", "cd", "def", "abcd"]) == True
+assert can_construct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]) == False
+assert can_construct("e" * 35 + "f", ["e" * i for i in range(1,8)]) == False
