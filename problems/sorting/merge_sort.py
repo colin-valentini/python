98f8a951178769538c13c9df6ebeae9b3e5582f9
Colin Valentini <valentini.colin@gmail.com>
2021-04-03 12:50:10 -0400

    Add optimized merge sort algorithm strategy

--- problems/sorting/merge_sort.py
+++ problems/sorting/merge_sort.py
@@ -1,4 +1,53 @@
-def merge_sort(array):
+from enum import Enum, auto
+
+class Strategy(Enum):
+  Optimal = auto()
+  Simple = auto()
+
+def merge_sort(array, strategy=Strategy.Optimal):
+  if strategy == Strategy.Optimal:
+    return _merge_sort_optimal(array)
+  elif strategy == Strategy.Simple:
+    return _merge_sort_simple(array)
+  raise KeyError(f'Invalid strategy: {strategy}') 
+
+def _merge_sort_optimal(array):
+  '''
+  Space optimized merge sort algorithm which performs the sorting in place.
+   - O(n*log(n)) time | O(n) space
+  '''
+  _merge_sort_in_place(array, array.copy(), 0, len(array)-1)
+  return array
+
+def _merge_sort_in_place(main, auxiliary, start, stop):
+  # Base case: array of length zero or one is trivially sorted in place
+  if start >= stop:
+    return
+  
+  mid = (start + stop) // 2
+  _merge_sort_in_place(auxiliary, main, start, mid)
+  _merge_sort_in_place(auxiliary, main, mid+1, stop)
+  _do_merge_in_place(main, auxiliary, start, mid, stop)
+
+def _do_merge_in_place(main, auxiliary, start, mid, stop):
+  i, j, k = start, mid + 1, start
+  while i <= mid and j <= stop:
+    if auxiliary[i] <= auxiliary[j]:
+      main[k] = auxiliary[i]
+      i += 1
+    else:
+      main[k] = auxiliary[j]
+      j += 1
+    k += 1
+  
+  while i <= mid:
+    main[k] = auxiliary[i]
+    i, k = i + 1, k + 1
+  while j <= stop:
+    main[k] = auxiliary[j]
+    j, k = j + 1, k + 1
+
+def _merge_sort_simple(array):
   '''
   Simple implementation of the merge sort algorithm which is not
   optimized for space. Sorting is NOT done in place!
@@ -13,9 +62,9 @@ def merge_sort(array):
 
   # Recursive calls all resolve to sorted sub-arrays of left and right
   # so we just need to merge them and return
-  return merge_sorted_arrays(merge_sort(left), merge_sort(right))
+  return _do_merge_simple(_merge_sort_simple(left), _merge_sort_simple(right))
 
-def merge_sorted_arrays(left, right):
+def _do_merge_simple(left, right):
   i, j = 0, 0
   merged_array = []
   while i < len(left) and j < len(right):
@@ -48,6 +97,7 @@ test_cases = [
 ]
 
 for test_case in test_cases:
-  result = merge_sort(test_case.copy())
-  expected = sorted(test_case)
-  assert result == expected
+  for strategy in Strategy:
+    result = merge_sort(test_case.copy(), strategy)
+    expected = sorted(test_case)
+    assert result == expected
